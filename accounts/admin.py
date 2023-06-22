afbb32ca8068b3d98c47d28a8d488a2f806d3a76
Kevin Bowen <kevin.bowen@gmail.com>
2023-06-21 20:51:40 -0700

    Register Profile model. Add profiles(#1)

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,1 +1,9 @@
-# Register your models here.
+from django.contrib import admin
+
+from .models import Profile
+
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ["user", "date_of_birth", "photo"]
+    raw_id_fields = ["user"]
