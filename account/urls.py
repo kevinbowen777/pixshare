7f31ee62f2e86b888762d9aea6e2582176652277
Kevin Bowen <kevin.bowen@gmail.com>
2023-07-03 16:43:01 -0700

    Wire up user_follow path(#24)

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,13 @@ from django.urls import include, path
 from . import views
 
 urlpatterns = [
+    path("", include("django.contrib.auth.urls")),
+    path("", views.dashboard, name="dashboard"),
+    path("register/", views.register, name="register"),
+    path("edit/", views.edit, name="edit"),
+    path("users/", views.user_list, name="user_list"),
+    path("users/follow/", views.user_follow, name="user_follow"),
+    path("users/<username>/", views.user_detail, name="user_detail"),
     # path("login/", views.user_login, name="login"),
     # path("login/", auth_views.LoginView.as_view(), name="login"),
     # path("logout/", auth_views.LogoutView.as_view(), name="logout"),
@@ -36,10 +43,4 @@ urlpatterns = [
     #     auth_views.PasswordResetCompleteView.as_view(),
     #     name="password_reset_complete",
     # ),
-    path("", include("django.contrib.auth.urls")),
-    path("", views.dashboard, name="dashboard"),
-    path("register/", views.register, name="register"),
-    path("edit/", views.edit, name="edit"),
-    path("users/", views.user_list, name="user_list"),
-    path("users/<username>/", views.user_detail, name="user_detail"),
 ]
