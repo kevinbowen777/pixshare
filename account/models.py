7d4509e450e20566c47f2b658e963296dd96aebc
Kevin Bowen <kevin.bowen@gmail.com>
2023-07-02 22:33:57 -0700

    Monkeypatch following field to User model(#24)

--- account/models.py
+++ account/models.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 
 
@@ -28,3 +29,13 @@ class Contact(models.Model):
 
     def __str__(self):
         return f"{self.user_from} follows {self.user_to}"
+
+
+# Add following field to User dynamically
+user_model = get_user_model()
+user_model.add_to_class(
+    "following",
+    models.ManyToManyField(
+        "self", through=Contact, related_name="followers", symmetrical=False
+    ),
+)
