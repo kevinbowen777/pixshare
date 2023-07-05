adaafa5d68555c8a142688c50cf5d6b2a2b9193e
Kevin Bowen <kevin.bowen@gmail.com>
2023-07-05 16:08:25 -0700

    Create templates for activity streams. Resolves #27

--- account/views.py
+++ account/views.py
@@ -43,7 +43,9 @@ def dashboard(request):
     if following_ids:
         # If user is following others, retriev only their actions
         actions = actions.filter(user_id__in=following_ids)
-    actions = actions[:10]
+    actions = actions.select_related("user", "user__profile").prefetch_related(
+        "target"
+    )[:10]
     return render(
         request,
         "account/dashboard.html",
@@ -55,19 +57,23 @@ def register(request):
     if request.method == "POST":
         user_form = UserRegistrationForm(request.POST)
         if user_form.is_valid():
-            # Create a new user object but avoid saving it yet
             new_user = user_form.save(commit=False)
-            # Set the chosen password
             new_user.set_password(user_form.cleaned_data["password"])
-            # Save the user object
             new_user.save()
-            # Create the user profile
             Profile.objects.create(user=new_user)
             create_action(new_user, "has created an account")
-            return render(request, "account/register_done.html", {"new_user": new_user})
+            return render(
+                request,
+                "account/register_done.html",
+                {"new_user": new_user},
+            )
     else:
         user_form = UserRegistrationForm()
-    return render(request, "account/register.html", {"user_form": user_form})
+    return render(
+        request,
+        "account/register.html",
+        {"user_form": user_form},
+    )
 
 
 @login_required
