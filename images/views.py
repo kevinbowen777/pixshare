88ee27091c3c8bfb16af79afa66a9be48e03c6f5
Kevin Bowen <kevin.bowen@gmail.com>
2023-07-10 17:34:03 -0700

    Establish redis connection & increment views(#32)

--- images/views.py
+++ images/views.py
@@ -1,4 +1,6 @@
+import redis
 from django.shortcuts import get_object_or_404, render, redirect
+from django.conf import settings
 from django.contrib.auth.decorators import login_required
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.decorators.http import require_POST
@@ -9,6 +11,13 @@ from actions.utils import create_action
 
 from .forms import ImageCreateForm
 from .models import Image
+
+
+r = redis.Redis(
+    host=settings.REDIS_HOST,
+    port=settings.REDIS_PORT,
+    db=settings.REDIS_DB,
+)
 
 
 @login_required
@@ -37,8 +46,11 @@ def image_create(request):
 
 def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
+    total_views = r.incr(f"image:{image.id}:views")
     return render(
-        request, "images/image/detail.html", {"section": "images", "image": image}
+        request,
+        "images/image/detail.html",
+        {"section": "images", "image": image, "total_views": total_views},
     )
 
 
